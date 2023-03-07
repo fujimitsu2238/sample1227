ad223d256593083da57c2a4b26ad298586d9d9eb
fujimitsu <you@example.com>
2023-03-07 10:16:20 +0000

    add 57_1

---
+++ datatype55_6.js
@@ -0,0 +1,18 @@
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"}
+  ];
+  
+  let user = users.find(item => item.id == 1);
+  
+  console.log(user.name);
+
+  let arry = [0,"0",()=>console.log("hello")]
+
+  function arry(item) {
+    return item.id ==1
+    
+  }
+
+  let user2 = {id: 1, name:"john"}
---
+++ datatype57_1.js
@@ -0,0 +1,26 @@
+let lesson ={ student: {name:"john" ,age: 20}} ;
+
+let obj2 ={student: 30                    };
+
+let student = {20,"30",40};
+
+console.log( lesson.student.name);
+
+console.log("In case of array",student[0]);
+
+let map = new Map();
+
+map.set('1','str1');
+map.set(1,'num1');
+map.set(true,'bool1');
+
+console.log(map.get(1));
+console.log(map.get('1'));
+
+console.log(map.size);
+
+
+
+
+
+
