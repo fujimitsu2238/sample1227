76e6ff45c10da817ead5c88e5435b33423b9af88
fujimitsu <you@example.com>
2023-03-28 10:29:32 +0000

    adddatatype512_1.js

---
+++ datatype512_1.js
@@ -0,0 +1,13 @@
+let user = {
+    name: "john",
+    age: 30
+};
+
+let json = JSON.stringify(user);
+
+
+let str = '{"name":"john","age":30}'
+
+console.log(str);
+console.log(json);
+console.log(typeof json);
