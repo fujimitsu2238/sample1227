4a2d80d580dc15c19c9af0b83a8f57bf4ac37cb7
fujimitsu <you@example.com>
2023-03-28 10:13:18 +0000

    add datatype510_1.js

---
+++ datatype510_1.js
@@ -0,0 +1,17 @@
+let options = {
+    title:"Menu",
+    width: 100,
+    height: 200
+};
+
+let arr = ["john","smith"];
+
+let [firstName,surname] = arr;
+
+let{title,width,height} = options;
+
+console.log(firstName);
+console.log(surname);
+
+console.log(title);
+console.log(width);
