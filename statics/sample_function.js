6861393dc4c05653fd8eef6d07575ff3f186d695
fujimitsu <you@example.com>
2023-11-14 11:08:37 +0000

    add function

---
+++ statics/sample_function.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+let userName = "john";
+
+function showMessae(from,text) {
+   
+    console.log(from);
+    console.log(text);
+   }
+
+function sayHi(){
+    let message = "Hi,I'm"
+    console.log(message,userName);
+}
+
+showMessae("Ann");
+
+
+sayHi();
